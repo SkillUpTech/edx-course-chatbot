584417ab7340c6a74f232e6af57ec4b50188ee27
samit7711 <samitp@skillup.tech>
2020-08-27 16:56:10 +0000

    Enabling and Disabling Chatbot Tab from admin changes

--- course_chatbot/tab.py
+++ course_chatbot/tab.py
@@ -1,22 +1,27 @@
 from django.conf import settings
 from django.utils.translation import ugettext_noop
-
 from courseware.tabs import EnrolledTab
 from courseware.access import has_access
+import waffle
 
 class ChatbotTab(EnrolledTab):
     """
     Defines the Wiki view type that is shown as a course tab.
     """
-
-    type = "chatbot"
-    title = ugettext_noop('Chatbot')
-    view_name = "course_chatbot"
-    is_dynamic = True
-
-    @classmethod
-    def is_enabled(cls, course, user=None):
+    
+    if waffle.switch_is_active('Chatbot_Switch'): 
         """
-        Returns true if the wiki is enabled and the specified user is enrolled or has staff access.
+        This Tab will be visible only when the Chatbot_Switch is active in Waffle Switches
         """
-        return bool(user and has_access(user, 'staff', course, course.id))
+        type = "chatbot"
+        title = ugettext_noop('Chatbot')
+        view_name = "course_chatbot"
+        is_dynamic = True
+
+        @classmethod
+        def is_enabled(cls, course, user=None):
+            """
+            Returns true if the wiki is enabled and the specified user is enrolled or has staff access.
+            """
+            return bool(user and has_access(user, 'staff', course, course.id))
+
--- course_chatbot/templatetags/course_chatbot_tags.py
+++ course_chatbot/templatetags/course_chatbot_tags.py
@@ -1,6 +1,7 @@
 from django import template
 from course_chatbot.models import CourseChatbot
 import requests
+import waffle
 
 register = template.Library()
 
@@ -9,7 +10,7 @@ def get_chatbot_url(course_id):
     """
     Get chatbot url for the course
     """
-    if "/courses/course-v1" in course_id:
+    if "/courses/course-v1" in course_id and waffle.switch_is_active('Chatbot_Switch'):
         cid = course_id.split("/courses/")[1].split("/")[0]
         try:
             course_chatbot = CourseChatbot.objects.get(course_id=cid)
@@ -22,4 +23,5 @@ def get_chatbot_url(course_id):
             return ""
     else:
         return ""
+
 
